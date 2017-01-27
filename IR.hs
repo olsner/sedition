7c8a0598c5bb89d54de0aa02f62e891de9074551
Simon Brenner <olsner@gmail.com>
2017-01-27 21:12:20 +0100

    Get rid of the old blocks stuff

--- IR.hs
+++ IR.hs
@@ -84,8 +84,6 @@ data IRState = State
   , sourceLabels :: Map S Label
   , nextCycleLabel :: Label
   , program :: Graph Insn O O
-  , cycleBlock :: BlockState
-  , block :: BlockState
   }
 
 instance Show (Graph Insn e x) where
@@ -93,8 +91,9 @@ instance Show (Graph Insn e x) where
 
 invalidLabel :: String -> Label
 invalidLabel s = error ("Invalid label: " ++ s)
-startState autoprint = State 0 1 autoprint M.empty (invalidLabel "uninitialized next-cycle label") emptyGraph Block0 BlockN
-p0 = Pred 0
+startState autoprint = State 0 2 autoprint M.empty (invalidLabel "uninitialized next-cycle label") emptyGraph
+pIntr = Pred 1
+pPreFirst = Pred 0
 
 instance UniqueMonad IRM where
   freshUnique = do
@@ -164,8 +163,8 @@ tIf p tx fx = do
     comment "tIf/end"
 
 ifCheck c tx fx = do
-  tCheck p0 c
-  tIf p0 tx fx
+  p <- tCheck c
+  tIf p tx fx
 
 type IRM = State IRState
 
@@ -184,72 +183,41 @@ tProgram seds = do
   intr <- newLabel
   exit <- newLabel
 
-  oldSourceLabels <- gets sourceLabels
-  oldCycleBlock <- gets cycleBlock
   oldNextCycle <- gets nextCycleLabel
 
-  modify $ \state -> state { nextCycleLabel = newCycle, cycleBlock = Block0, sourceLabels = M.empty }
-
-  tSeds seds
-
-  emitLabel newCycle
-  get >>= \s -> when (autoprint s) (emit (Print 0))
-  emit Clear
-  doRead <- newLabel
-  emit (AtEOF p0)
-  If p0 exit doRead `thenLabel` doRead
-  Read 0 (Just intr) entry `thenLabel` entry
-
-  modify $ \state -> state { nextCycleLabel = newCycle, cycleBlock = BlockN, sourceLabels = M.empty }
+  modify $ \state -> state { nextCycleLabel = newCycle }
+  emit (Set pPreFirst True)
+  emitLabel entry
   -- Actual normal program here
   tSeds seds
 
-  Branch newCycle `thenLabel` intr
+  emitLabel newCycle
+  emit (Set pPreFirst False)
+  emit (Set pIntr False)
+  get >>= \s -> when (autoprint s) (emit (Print 0))
+  emit Clear
+  doRead <- newLabel
+  p <- withNewPred $ \p -> emit (AtEOF p)
+  If p exit doRead `thenLabel` doRead
+  Read 0 (Just intr) entry `thenLabel` intr
 
-  modify $ \state -> state { nextCycleLabel = newCycle, cycleBlock = BlockI, sourceLabels = M.empty }
-  tSeds seds
+  emit (Set pIntr True)
 
-  Branch newCycle `thenLabel` exit
+  Branch entry `thenLabel` exit
 
-  modify $ \state -> state { nextCycleLabel = oldNextCycle, cycleBlock = oldCycleBlock, sourceLabels = oldSourceLabels }
+  modify $ \state -> state { nextCycleLabel = oldNextCycle }
 
 tSeds = mapM_ tSed
 
-withBlock b x = do
-  oldBlock <- block <$> get
-  modify $ \s -> s { block = b }
-  cblock <- gets cycleBlock
-  comment ("start of block " ++ show b ++ " in " ++ show cblock)
-  x
-  cblock <- gets cycleBlock
-  comment ("end of block " ++ show b ++ " in " ++ show cblock)
-  modify $ \s -> s { block = oldBlock }
-
-blockForAddr AST.IRQ = BlockI
-blockForAddr (AST.Line 0) = Block0
-blockForAddr _ = BlockN
-
-ifBlock b x = do
-  block <- gets cycleBlock
-  when (b == block) x
-  when (b /= block) $ do
-    comment ("wrong block " ++ show block ++ ", wanted " ++ show b)
-
-ifRightBlock x = do
-  b <- gets block
-  ifBlock b x
-
-withCond Always x = ifRightBlock x
+withCond Always x = x
 withCond (At c) x = do
-  block <- gets cycleBlock
-  when (blockForAddr c == block) $ do
-    f <- newLabel
-    t <- newLabel
-    tCheck p0 c
-    finishBlock (If p0 t f) t
-    withBlock (blockForAddr c) x
-    emitLabel f
-withCond (Between start end) x = ifBlock BlockN $ do
+  f <- newLabel
+  t <- newLabel
+  p <- tCheck c
+  finishBlock (If p t f) t
+  x
+  emitLabel f
+withCond (Between start end) x = do
   pActive <- newPred
   f <- newLabel
   t <- newLabel
@@ -271,21 +239,22 @@ withCond (Between start end) x = ifBlock BlockN $ do
   comment "between/end of code"
   emitLabel f
 
--- TODO Even in "block 0", this will stop being true if we read a line...
-tCheck p (AST.Line 0) = do
-  b <- gets cycleBlock
-  emit (Set p (b == Block0))
-tCheck p (AST.Line n) = emit (Line n p)
-tCheck p (AST.Match (Just re)) = emit (Match re p) >> emit (SetLastRE re)
-tCheck p (AST.Match Nothing) = emit (MatchLastRE p)
-tCheck p addr = error ("tCheck: Unmatched case " ++ show addr)
+withNewPred f = newPred >>= \p -> f p >> return p
+
+tCheck (AST.Line n) = withNewPred $ \p -> emit (Line n p)
+tCheck (AST.Match (Just re)) = withNewPred $ \p -> do
+    emit (Match re p)
+    emit (SetLastRE re)
+tCheck (AST.Match Nothing) = withNewPred $ \p -> emit (MatchLastRE p)
+tCheck (AST.IRQ) = return pIntr
+tCheck addr = error ("tCheck: Unmatched case " ++ show addr)
 
 tSed (Sed Always (AST.Block xs)) = tSeds xs
-tSed (Sed cond (AST.Block xs)) = withCond cond $ tSeds xs
-tSed (Sed cond x) = do
-  cb <- gets cycleBlock
-  comment ("In " ++ show cb ++ ", " ++ show cond ++ ": " ++ show x)
-  withCond cond $ tCmd x
+-- TODO Always for other things: check that not prefirst or interrupt
+-- TODO Figure out how to do jumps out of and into such blocks thouhg...
+-- Separate run-all-blocks flag? Cleared by default, gets set when entering an
+-- interrupt or pre-first block?
+tSed (Sed cond x) = withCond cond $ tCmd x
 
 tCmd (AST.Block xs) = tSeds xs
 tCmd (AST.Print fd) = emit (Print fd)
@@ -321,8 +290,8 @@ tCmd (AST.Clear) = emit Clear
 tCmd (AST.Redirect dst (Just src)) = emit (Redirect dst src)
 tCmd (AST.Redirect dst Nothing) = emit (CloseFile dst)
 tCmd (AST.Subst mre sub flags actions) = do
-  tCheck p0 (AST.Match mre)
-  tIf p0 (emit (Subst sub flags) >> tSubstAction actions) (return ())
+  p <- tCheck (AST.Match mre)
+  tIf p (emit (Subst sub flags) >> tSubstAction actions) (return ())
 tCmd (AST.Hold maybeReg) = emit (Hold maybeReg)
 tCmd (AST.Get maybeReg) = emit (Get maybeReg)
 tCmd (AST.GetA maybeReg) = emit (GetA maybeReg)
