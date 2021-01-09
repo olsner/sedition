a192b1d0069a6d4c42dca44dcb1a141da2a0df81
Simon Brenner <olsner@gmail.com>
2021-01-09 13:52:00 +0100

    Add way to run specific gnused tests with more output

--- run-gnused-tests.sh
+++ run-gnused-tests.sh
@@ -22,22 +22,35 @@ set -e
 test -d "$1" || usage
 
 gnused=$(realpath "$1")
+shift
+
 export abs_top_srcdir=$gnused
-export abs_top_srcdir=$gnused
+export srcdir=$gnused
 export SED=`pwd`/sed
+
+if [ $# -gt 0 ]; then
+    tests=( "$@" )
+    quiet=
+else
+    tests=( $gnused/testsuite/*.sed )
+    quiet="&>/dev/null"
+fi
 
 fails=0
 passes=0
 
-cd $gnused
+cd $gnused/testsuite
 test -x "$SED"
 # TODO Are there more extensions for sed scripts here?
-for t in testsuite/*.sed; do
+for t in "${tests[@]}"; do
     name=$(basename -s .sed "$t")
-    if testsuite/runtest "$name" &>/dev/null; then
+    if eval './runtest "$name" '$quiet; then
         addpass "$name"
     else
         addfail "$name"
+        if [ -n "$verbose" -o ${#tests[@]} -eq 1 ]; then
+            diff -u $name.good $name.out || true
+        fi
     fi
 done
 
