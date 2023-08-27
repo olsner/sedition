c029f2dfdf85e7e3403dd994ebbbc2961b7ff279
Simon Brenner <olsner@gmail.com>
2023-08-27 11:58:23 +0200

    Clean up the unused implementation of CharMap

--- CharMap.hs
+++ CharMap.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE TupleSections #-}
-
 module CharMap (
     CharMap(),
     -- construction
@@ -18,10 +15,6 @@ module CharMap (
     union
     ) where
 
-import Data.List (sort)
-
-import Data.Map (Map)
-import qualified Data.Map as M
 import Data.Set (Set)
 import qualified Data.Set as S
 import Data.Word (Word8)
@@ -29,75 +22,7 @@ import Data.Word (Word8)
 import RangeMap (RangeMap)
 import qualified RangeMap as RM
 
-#if 0
-
--- Make this e.g. RangeMap k v, then add CharMap a = RangeMap Char a
-newtype CharMap a = CharMap (Map Char a) deriving (Show, Ord, Eq)
-
-empty = CharMap (M.empty)
-singleton k v = insert k v empty
-
-null (CharMap m) = M.null m
-
-elems (CharMap m) = M.elems m
-elemSet (CharMap m) = S.fromList (M.elems m)
-
-backward :: Ord a => CharMap a -> Map a [Char]
-backward (CharMap m) = foldr multiMapInsert M.empty (M.toList m)
-
-multiMapInsert :: Ord a => (Char, a) -> Map a [Char] -> Map a [Char]
-multiMapInsert (k,v) = M.insertWith (<>) v [k]
-
-lookup :: Char -> CharMap a -> Maybe a
-lookup k (CharMap m) = M.lookup k m
-
-findWithDefault :: a -> Char -> CharMap a -> a
-findWithDefault def k (CharMap m) = M.findWithDefault def k m
-
-toList :: Ord a => CharMap a -> [([Char], a)]
-toList m = [(ks, v) | (v,ks) <- M.toList (backward m)]
-
-toRanges :: Ord a => CharMap a -> [([(Char, Char)], a)]
-toRanges = map (\(cs, a) -> (checkRanges cs (ranges cs), a)) . toList
-  where
-    -- Since cs comes from M.toList it should already be sorted!
-    ranges :: [Char] -> [(Char,Char)]
-    ranges cs | cs /= sort cs = error "double-check failed"
-    ranges [] = []
-    ranges (c:cs) = go c c cs
-    go :: Char -> Char -> [Char] -> [(Char,Char)]
-    go min max (c:cs) | c == succ max = go min c cs
-    go min max cs = (min,max) : ranges cs
-
-    checkRanges cs rs | cs == expandRanges rs = rs
-                      | otherwise = error "expandRanges check failed"
-    expandRanges [] = []
-    expandRanges ((min,max):cs) = [min..max] ++ expandRanges cs
-
-fromList = CharMap . M.fromList
-
-insert :: Char -> a -> CharMap a -> CharMap a
-insert k v (CharMap m) = CharMap (M.insert k v m)
-
-delete k (CharMap m) = CharMap (M.delete k m)
-
-allChars = ['\0'..'\xff']
-
--- Cheating here - Char has a large range but we'll work like Char8 here.
-isComplete (CharMap m) = S.toList (M.keysSet m) == allChars
-
-traverse :: (Ord a, Applicative f) => (a -> f b) -> CharMap a -> f (CharMap b)
-traverse f cm = fromList . concat <$> Prelude.traverse f' (toList cm)
-  where
-    f' (ks,v) = (\v' -> (,v') <$> ks) <$> f v
-
-const def = fromList [(k, def) | k <- allChars]
-
-union (CharMap m) (CharMap n) = CharMap (M.union m n)
-
-#else
-
-type CharMap a = RangeMap Word8 a -- deriving (Show, Ord, Eq)
+type CharMap a = RangeMap Word8 a
 
 w8 :: Char -> Word8
 w8 = toEnum . fromEnum
@@ -145,5 +70,3 @@ const = RM.const
 
 union :: (Ord a, Show a) => CharMap a -> CharMap a -> CharMap a
 union = RM.union
-
-#endif
